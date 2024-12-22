e283a28500f47cc2ee7714344267e0259aa3ca6c
Güneş Balcı <gunesbalci66@hotmail.com>
2024-12-22 23:04:40 +0300

    LOG package and SignInOutLog class are created.
    Log sign in and log sign out functions are completed.

---
+++ src/LOG/SignInOut_LOG.java
@@ -0,0 +1,70 @@
+package LOG;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class SignInOut_LOG
+{
+    private static File SignInOutLogs;
+
+    public static void InitializeFile()
+    {
+        try
+        {
+            SignInOutLogs = new File("src/LOG/Files/SignInOutLOG.txt");
+            SignInOutLogs.createNewFile();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void LogSignIn(String id)
+    {
+        String formattedDateTime = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+        try
+        {
+            FileWriter fileWriter = new FileWriter(SignInOutLogs);
+
+            String LogString = id + "...";
+            LogString += "signIn" + "...";
+            LogString += formattedDateTime;
+
+            fileWriter.write(LogString);
+            fileWriter.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void LogSignOut(String id)
+    {
+        String formattedDateTime = ZonedDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
+        try
+        {
+            FileWriter fileWriter = new FileWriter(SignInOutLogs);
+
+            String LogString = id + "...";
+            LogString += "signOut" + "...";
+            LogString += formattedDateTime;
+
+            fileWriter.write(LogString);
+            fileWriter.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import GUI.GUI;
+import LOG.SignInOut_LOG;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        SignInOut_LOG.InitializeFile();
         GUI.InitializeFrame();
     }
 }
