5ec65d17aebd4227423d456dd1d1114fed994952
Güneş Balcı <gunesbalci66@hotmail.com>
2024-12-27 17:03:50 +0300

    Process destroy is added.

--- src/MultiProcess/Process.java
+++ src/MultiProcess/Process.java
@@ -1,29 +1,37 @@
 package MultiProcess;
 
 import java.io.File;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Process
 {
-    public static void StartSignInOut()
+    static java.lang.Process process_Signinout;
+    static java.lang.Process process_Backup;
+    static java.lang.Process process_Watch;
+
+    static ScheduledExecutorService scheduler_SignInOut;
+    static ScheduledExecutorService scheduler_Backup;
+
+
+    public static java.lang.Process StartSignInOut()
     {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler_SignInOut = Executors.newScheduledThreadPool(1);
 
         Runnable task = () ->
         {
             try
             {
-                ProcessBuilder process = new ProcessBuilder(
+                ProcessBuilder processBuilder = new ProcessBuilder(
                         "java", "-cp",
                         System.getProperty("java.class.path"),
                         "LOG.SignInOut_LOG");
 
-                process.directory(new File("C:\\3.Yıl\\1.Dönem\\YazılımGeliştirme1\\FileShareSystem"));
-                process.inheritIO();
-                process.start();
+                processBuilder.directory(new File("C:\\3.Yıl\\1.Dönem\\YazılımGeliştirme1\\FileShareSystem"));
+                processBuilder.inheritIO();
+                process_Signinout = processBuilder.start();
             }
             catch (Exception e)
             {
@@ -31,25 +39,26 @@ public class Process
             }
         };
 
-        scheduler.scheduleAtFixedRate(task, 0, 5, TimeUnit.SECONDS);
+        scheduler_SignInOut.scheduleAtFixedRate(task, 0, 5, TimeUnit.SECONDS);
+        return process_Signinout;
     }
 
-    public static void StartBackup()
+    public static java.lang.Process StartBackup()
     {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler_Backup = Executors.newScheduledThreadPool(1);
 
         Runnable task = () ->
         {
             try
             {
-                ProcessBuilder process = new ProcessBuilder(
+                ProcessBuilder processBuilder = new ProcessBuilder(
                         "java", "-cp",
                         System.getProperty("java.class.path"),
                         "AppFile.BackupFiles");
 
-                process.directory(new File("C:\\3.Yıl\\1.Dönem\\YazılımGeliştirme1\\FileShareSystem"));
-                process.inheritIO();
-                process.start();
+                processBuilder.directory(new File("C:\\3.Yıl\\1.Dönem\\YazılımGeliştirme1\\FileShareSystem"));
+                processBuilder.inheritIO();
+                process_Backup = processBuilder.start();
             }
             catch (Exception e)
             {
@@ -57,33 +66,29 @@ public class Process
             }
         };
 
-        scheduler.scheduleAtFixedRate(task, 0, 5, TimeUnit.SECONDS);
+        scheduler_Backup.scheduleAtFixedRate(task, 0, 5, TimeUnit.SECONDS);
+        return process_Backup;
     }
 
-    public static void StartWatcher()
+    public static java.lang.Process StartWatcher()
     {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
-        Runnable task = () ->
+        try
         {
-            try
-            {
-                ProcessBuilder process = new ProcessBuilder(
-                        "java", "-cp",
-                        System.getProperty("java.class.path"),
-                        "AppFile.FileWatcher");
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "java", "-cp",
+                    System.getProperty("java.class.path"),
+                    "AppFile.FileWatcher");
 
-                process.directory(new File("C:\\3.Yıl\\1.Dönem\\YazılımGeliştirme1\\FileShareSystem"));
-                process.inheritIO();
-                process.start();
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-        };
+            processBuilder.directory(new File("C:\\3.Yıl\\1.Dönem\\YazılımGeliştirme1\\FileShareSystem"));
+            processBuilder.inheritIO();
+            process_Watch = processBuilder.start();
+        }
+        catch (RuntimeException | IOException e)
+        {
+            throw new RuntimeException(e);
+        }
 
-        scheduler.scheduleAtFixedRate(task, 0, 5, TimeUnit.SECONDS);
+        return process_Watch;
     }
 
     public static void StartAllProcess()
@@ -91,5 +96,30 @@ public class Process
         StartSignInOut();
         StartBackup();
         StartWatcher();
+    }
+
+    public static void KillAllProcess()
+    {
+        if (scheduler_SignInOut != null)
+        {
+            scheduler_SignInOut.shutdownNow();
+        }
+        if (scheduler_Backup != null)
+        {
+            scheduler_Backup.shutdownNow();
+        }
+        if (process_Signinout != null)
+        {
+            process_Signinout.destroy();
+        }
+        if (process_Backup != null)
+        {
+            process_Backup.destroy();
+        }
+        if (process_Watch != null)
+        {
+            process_Watch.destroy();
+        }
+        System.out.println("öldürdüm");
     }
 }
